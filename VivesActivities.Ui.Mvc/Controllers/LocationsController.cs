1887f61be195cc837f7d88d333f9b584d1e8b5e5
mustafafawzi <114569862+mustafafawzi@users.noreply.github.com>
2023-12-15 11:35:37 +0100

    Initial commit

---
+++ VivesActivities.Ui.Mvc/Controllers/LocationsController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesActivities.Model;
+using VivesActivities.Services;
+
+namespace VivesActivities.Ui.Mvc.Controllers
+{
+    public class LocationsController(LocationService locationService) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var locations = locationService.Find();
+            return View(locations);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(location);
+            }
+
+            locationService.Create(location);
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var location = locationService.Get(id);
+
+            if (location is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(location);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(location);
+            }
+
+            locationService.Update(id, location);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var location = locationService.Get(id);
+
+            if (location is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(location);
+        }
+
+        [HttpPost("[controller]/delete/{id:int?}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            locationService.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VivesActivities.Ui.Mvc/Controllers/VivesActivitiesController.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using VivesActivities.Model;
+using VivesActivities.Services;
+
+namespace VivesActivities.Ui.Mvc.Controllers
+{
+    public class VivesActivitiesController(
+        VivesActivityService activityService,
+        LocationService locationService) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var activities = activityService.Find();
+            return View(activities);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return VivesActivityView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(VivesActivity activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return VivesActivityView(activity);
+            }
+
+            activityService.Create(activity);
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var activity = activityService.Get(id);
+
+            if (activity is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return VivesActivityView(activity);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, VivesActivity activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return VivesActivityView(activity);
+            }
+
+            activityService.Update(id, activity);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var activity = activityService.Get(id);
+
+            if (activity is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(activity);
+        }
+
+        [HttpPost("[controller]/delete/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            activityService.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+        
+        private IActionResult VivesActivityView(VivesActivity? activity = null, [CallerMemberName] string? viewName = null)
+        {
+            var locations = locationService.Find();
+            ViewBag.Locations = locations;
+
+            if (activity is null)
+            {
+                return View(viewName);
+            }
+
+            return View(viewName, activity);
+        }
+    }
+}
